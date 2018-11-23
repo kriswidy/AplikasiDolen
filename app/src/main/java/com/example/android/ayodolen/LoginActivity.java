54b8d9f37c599e736b73f948ad21b880f6c1fd5c
diosyahrizal <diosyahrizal@gmail.com>
2018-11-23 08:47:25 +0700

    gradle fixxxx

--- app/src/main/java/com/example/android/ayodolen/LoginActivity.java
+++ app/src/main/java/com/example/android/ayodolen/LoginActivity.java
@@ -14,17 +14,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-<<<<<<< HEAD
         //getActionBar().hide();
-
         username = findViewById(R.id.editTextPassword);
         password = findViewById(R.id.editTextPassword);
-
         btnLogin = findViewById(R.id.buttonYuk);
-
-
-=======
 //        getActionBar().hide();
->>>>>>> f6d19b937c0ef3d46c9dcb1c0964f00c1a6193e8
+
     }
 }
