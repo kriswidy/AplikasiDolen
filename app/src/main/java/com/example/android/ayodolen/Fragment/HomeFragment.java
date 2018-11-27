fb44211ac4556aad8e145d4a2592e29990a824b6
pradikaaaaa <pradikanizer13@gmail.com>
2018-11-27 10:20:15 +0700

    tambhan fragment

---
+++ app/src/main/java/com/example/android/ayodolen/Fragment/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.ayodolen.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.android.ayodolen.R;
+
+/**
+ * Created by user on 27/11/2018.
+ */
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+
+}
