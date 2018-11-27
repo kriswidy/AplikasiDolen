afda06c3602e4a27e272893dc8913784ca26d238
ilbad <ilbaddzulfadli98@gmail.com>
2018-11-27 09:58:43 +0700

    cbregister

---
+++ app/src/main/java/com/example/android/ayodolen/DataHelper.java
@@ -0,0 +1,29 @@
+package com.example.android.ayodolen;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "user.db";
+    private static final int DATABASE_VERSION = 2;
+
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE user ( email text primary key , username text null, password text);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
+        onCreate(db);
+    }
+}
