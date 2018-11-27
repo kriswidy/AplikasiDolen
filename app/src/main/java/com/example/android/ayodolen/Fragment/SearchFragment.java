4d2214364290fe97a578a5c8b425b33c1a60f048
pradikaaaaa <pradikanizer13@gmail.com>
2018-11-27 21:19:05 +0700

    profile fragment

--- app/src/main/java/com/example/android/ayodolen/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/android/ayodolen/Fragment/SearchFragment.java
@@ -1,14 +1,21 @@
 package com.example.android.ayodolen.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.ayodolen.R;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 /**
  * Created by user on 27/11/2018.
@@ -16,11 +23,54 @@ import com.example.android.ayodolen.R;
 
 public class SearchFragment extends Fragment {
 
+    MaterialSearchView searchView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search,container,false);
+        View view = inflater.inflate(R.layout.fragment_search,container,false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbarSeacrh);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Cari Wisata");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        setHasOptionsMenu(true);
+
+        searchView = view.findViewById(R.id.search_view);
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+            }
+        });
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //kalo diiisi
+                return false;
+            }
+        });
+
+
+        return view;
     }
 
-
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_search,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+    }
 }
