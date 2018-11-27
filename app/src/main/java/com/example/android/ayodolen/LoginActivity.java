f5cec3039018ab9f0b614be580df4cc8691d7873
pradikaaaaa <pradikanizer13@gmail.com>
2018-11-27 10:22:32 +0700

    fragment

--- app/src/main/java/com/example/android/ayodolen/LoginActivity.java
+++ app/src/main/java/com/example/android/ayodolen/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.ayodolen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,6 +21,14 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.editTextPassword);
         btnLogin = findViewById(R.id.buttonYuk);
 //        getActionBar().hide();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/android/ayodolen/RegisterActivity.java
+++ app/src/main/java/com/example/android/ayodolen/RegisterActivity.java
@@ -1,14 +1,62 @@
 package com.example.android.ayodolen;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    DataHelper dbHelper;
+    Button register;
+    TextView login;
+    EditText email, username, pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 //        getActionBar().hide();
+
+        email = findViewById(R.id.inputEmail);
+        username = findViewById(R.id.inputUsername);
+        pwd = findViewById(R.id.inputPasswd);
+
+        register = findViewById(R.id.btnRegister);
+        login = findViewById(R.id.tvLogin);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("INSERT INTO user ( email, username, password) VALUES ('" +
+                        email.getText().toString() + "','" +
+                        username.getText().toString() + "','" +
+                        pwd.getText().toString() + "')");
+
+                Toast.makeText(getApplicationContext(), "Register Successful", Toast.LENGTH_SHORT).show();
+                emptyEditText();
+//                Intent mIntent = new Intent(getApplicationContext(), LoginActivity.class);
+//                startActivity(mIntent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void emptyEditText(){
+        email.setText(null);
+        username.setText(null);
+        pwd.setText(null);
     }
 }
