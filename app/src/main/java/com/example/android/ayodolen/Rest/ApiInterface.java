b2f1bcfbe048a546caae069eabc060583c9a9c2a
diosyahrizal <diosyahrizal@gmail.com>
2018-11-29 17:05:09 +0700

    login sudah

--- app/src/main/java/com/example/android/ayodolen/Rest/ApiInterface.java
+++ app/src/main/java/com/example/android/ayodolen/Rest/ApiInterface.java
@@ -1,10 +1,21 @@
 package com.example.android.ayodolen.Rest;
 
+import com.example.android.ayodolen.Model.User;
+import com.example.android.ayodolen.Model.UserResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * Created by user on 15/11/2018.
  */
 
 public interface ApiInterface {
+    @FormUrlEncoded
+    @POST("login")
+    Call<UserResponse> loginRequest(@Field("username") String username);
 //    @FormUrlEncoded
 //    @POST("login")
 //    Call<UserResponse> loginRequest(@Field("username") String username,
