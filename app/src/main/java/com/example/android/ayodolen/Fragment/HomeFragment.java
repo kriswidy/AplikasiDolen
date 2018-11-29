e034528033c14aa34b102ee8994e57acff8b0964
wildanal2-Pc <wildanal2@gmail.com>
2018-11-29 18:09:30 +0700

    desain beranda

--- app/src/main/java/com/example/android/ayodolen/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/android/ayodolen/Fragment/HomeFragment.java
@@ -9,16 +9,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.example.android.ayodolen.R;
 
+import technolifestyle.com.imageslider.FlipperLayout;
+import technolifestyle.com.imageslider.FlipperView;
+
 /**
  * Created by user on 27/11/2018.
  */
 
 public class HomeFragment extends Fragment {
+    FlipperLayout flipper;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        flipper = view.findViewById(R.id.flipper);
+
+        setLayout();
+
+        return view;
+    }
+
+    private void setLayout(){
+        String url [] = new String[]{
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Malang_WEB.jpg/1200px-Malang_WEB.jpg",
+                "https://pergimulu.com/wp-content/uploads/2017/03/Panduan-Tips-Pergi-Liburan-Ke-Malang-via-@candra_ea.jpg",
+                "https://img.ayokuliah.id/uploads/2015/09/Politeknik-Negeri-Malang.jpg"
+
+        };
+
+        for(int i=0; i<3; i++){
+            FlipperView fv = new FlipperView(getActivity().getBaseContext());
+            fv.setImageUrl(url[i])
+                    .setDescription("Image"+(i+1));
+            flipper.addFlipperView(fv);
+        }
     }
 
 
