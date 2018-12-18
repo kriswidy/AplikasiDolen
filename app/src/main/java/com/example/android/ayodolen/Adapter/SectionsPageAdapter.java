4dc8fa8215339adeb14635719f80b7f728fc0d35
pradikaaaaa <pradikanizer13@gmail.com>
2018-12-19 00:20:58 +0700

    komentar v4

--- app/src/main/java/com/example/android/ayodolen/Adapter/SectionsPageAdapter.java
+++ app/src/main/java/com/example/android/ayodolen/Adapter/SectionsPageAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.ayodolen.Adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
--- app/src/main/java/com/example/android/ayodolen/Fragment/KomentarFragment.java
+++ app/src/main/java/com/example/android/ayodolen/Fragment/KomentarFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -87,6 +88,10 @@ public class KomentarFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 insertData(s1.getKeyId());
+                edtkomentar.setText("");
+
+
+
 
             }
         });
